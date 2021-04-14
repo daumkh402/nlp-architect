9000996409be5031058a3536a466a94dea6b4a28
marue <daumkh402@naver.com>
2021-04-15 01:00:22 +0900

    update

--- bert_config.sh
+++ bert_config.sh
@@ -2,11 +2,15 @@ mkdir ../bert_model
 mkdir ../Qbert_model
 cp bert_config.json ../bert_model
 cp Qbert_config.json ../Qbert_model
+
 cd ../bert_model
 wget https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-uncased-vocab.txt
 wget https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-uncased-pytorch_model.bin
 mv bert-base-uncased-vocab.txt vocab.txt
 mv bert-base-uncased-pytorch_model.bin pytorch_model.bin
+mv bert_config.json config.json
+mv ../Qbert_model/Qbert_config.json ../Qbert_model/config.json
+
 
 cp vocab.txt ../Qbert_model
 cp pytorch_model.bin ../Qbert_model
